4a80e8bcd3eb11ecf76a394deb5a7bb891e6e67b
Pete <github@kthxbye.us>
2025-03-02 23:56:07 +0100

    ADD: blacklist to postprocessing

--- postprocess.py
+++ postprocess.py
@@ -14,8 +14,24 @@ def filter_qxy(lines : list) -> list:
 
     return filtered_list
 
+def filter_blacklist(lines, blacklist):
+    blacklist = [word.lower() for word in blacklist]
+    filtered_list = []
+    for line in lines:
+        blacklisted = False
+        for word in line.split(" ")[:5]:
+            if word.lower() in blacklist:
+                print(f"Skipping {word} because in blacklist")
+                blacklisted = True
+                continue
+        if not blacklisted:
+            filtered_list.append(line)
+
+    return filtered_list
 
 if __name__ == "__main__":
     results = utils.read_file_lines("results.txt")
     results_qxy_filtered = filter_qxy(results)
+    results_blacklist_filtered = filter_blacklist(results, utils.read_file_lines("words/blacklist/blacklist.txt"))
+    utils.write_file_lines("results_after_blacklist.txt", results_blacklist_filtered)
     utils.write_file_lines("results_qxy_filtered.txt", results_qxy_filtered)
