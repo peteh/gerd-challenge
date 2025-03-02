be1984ceb586f56586e7614e701ab761ac1a8f38
Pete <github@kthxbye.us>
2025-03-02 19:01:58 +0100

    CHG: bit of formatting

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ class Combiner():
         num_combinations = math.factorial(num_words) // math.factorial(num_words - r)
         num_processed = 0
         results_file = open("results.txt", "w")
-        
+
         for word1_index in range(0, num_words):
             word1 = self._words[word1_index]
             for word2_index in range(word1_index+1, num_words):
@@ -24,24 +24,27 @@ class Combiner():
                 if utils.count_used_alphabet_letters(word1+word2) < 10:
                     # skip if bad
                     continue
-                
+
                 for word3_index in range(word2_index+1, num_words):
                     word3 = self._words[word3_index]
                     if utils.count_used_alphabet_letters(word1+word2+word3) < 15:
                         # skip if bad
                         continue
-                    
+
                     for word4_index in range(word3_index+1, num_words):
                         word4 = self._words[word4_index]
                         if utils.count_used_alphabet_letters(word1+word2+word3+word4) < 20:
                             # skip if bad
                             continue
+
                         for word5_index in range(word4_index+1, num_words):
                             word5 = self._words[word5_index]
                             eval_combined = word1 + word2 + word3 + word4 + word5
                             num_processed += 1
-                            if num_processed % 1000000 == 0: 
+                            if num_processed % 1000000 == 0:
                                 print(f"Processed: {num_processed}/{num_combinations}")
+
+                            # Write words to file if we have enough alphabet letters
                             alphabet_letters = utils.count_used_alphabet_letters(eval_combined)
                             if alphabet_letters >= 24:
                                 line = f"{word1.upper()} {word2.upper()} {word3.upper()} {word4.upper()} {word5.upper()} ({alphabet_letters})\n" 
